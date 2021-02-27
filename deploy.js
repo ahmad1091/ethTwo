ab61cacaa68a2d1c97279943ac2328103f729b60
ahmad1091 <ahmadtayeb91@gmail.com>
2021-02-27 09:53:56 +0200

    create deployment code

---
+++ deploy.js
@@ -0,0 +1,43 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledContract = require("./compile");
+
+const bytecode = compiledContract.evm.bytecode.object;
+const abi = compiledContract.abi;
+
+const mnemonicPhrase =
+  "carbon tower feel armed margin furnace nothing false course nature glance coyote";
+
+let provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: mnemonicPhrase,
+  },
+  providerOrUrl:
+    "https://rinkeby.infura.io/v3/190d2e2670274a3183a86804ddee1ee3",
+});
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  try {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log("extracted accounts", accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+      .deploy({
+        data: bytecode,
+      })
+      .send({
+        gas: "1000000",
+        from: accounts[0],
+      });
+
+    console.log("Adress =>", result.options.address);
+    process.exit(1);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+deploy();
